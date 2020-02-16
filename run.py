0feb7d2c822fb16491b7c164e11535242eff9532
reSHARMA <reshabhsh@gmail.com>
2020-02-16 15:29:24 +0300

    Add a python wrapper to launch riscv size and perf benchmarks

---
+++ run.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+import sys
+import os
+import subprocess
+
+def main():
+
+    parser = ArgumentParser(description="Run rv8-bench")
+    parser.add_argument('-cc', '--c_compiler',
+            action='store',
+            help='Name of the compiler',
+            required=False)
+    parser.add_argument('-cxx', '--cxx_compiler',
+            action='store',
+            help='Name of the compiler',
+            required=False)
+    try:
+        args = parser.parse_args()
+        print(args)
+
+    except:
+        parser.error("Invalid Options.")
+        sys.exit(1)
+
+    cCompiler = "gcc"
+    cxxCompiler = "g++"
+    if args.c_compiler:
+        cCompiler= args.c_compiler
+    if args.cxx_compiler:
+        cxxCompiler= args.cxx_compiler
+
+
+    runrv8Bench(cCompiler, cxxCompiler, args.mount_path)
+
+def setupSymlinks(cc, cxx):
+    toolchains = ["riscv32-linux-musl-", "riscv64-linux-musl-", "i386-linux-musl-", "x86_64-linux-musl-", "arm-linux-musleabihf-", "aarch64-linux-musl-"]
+    prefix = cc.split("-")
+    prefix.pop()
+    strip = "-".join(prefix) + "-strip"
+    for toolchain in toolchains:
+            subprocess.run(["ln", "-sf", cc, "/usr/bin/" + toolchain + "gcc"], check = True)
+            subprocess.run(["ln", "-sf", cxx, "/usr/bin/" + toolchain + "g++"], check = True)
+            subprocess.run(["ln", "-sf", strip, "/usr/bin/" + toolchain + "strip"], check = True)
+
+
+def runrv8Bench(cCompiler, cxxCompiler, mountPath):
+    setupSymlinks(cCompiler, cxxCompiler)
+    subprocess.run(['apt-get', '-y', 'install', 'nodejs', 'npm'], check=True)
+    subprocess.run("curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -", shell=True, check = True)
+    subprocess.run(['git', 'clone', 'https://github.com/rv8-io/rv8.git', '||', 'true'], check = True)
+    subprocess.run(['git', 'submodule', 'update', '--init'], check = True, cwd = "rv8")
+    subprocess.run(['make', '-j24'], check = True, cwd = "rv8")
+    subprocess.run(['make', 'install'], check = True, cwd = "rv8")
+    subprocess.run(['make'], check = True)
+    subprocess.run(['npm', 'start', 'bench', 'all', 'qemu-riscv64', 'O3', '1'], check = True)
+    subprocess.run(['npm', 'start', 'bench', 'all', 'size-riscv64', 'Os', '1'], check = True)
+
+if __name__ == "__main__":
+    main()
